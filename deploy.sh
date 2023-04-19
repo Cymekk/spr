938c6945cfafe9043d16e684267059b4251107b9
Marcin Cymerman <cymerman944@gmail.com>
2023-04-19 11:28:05 +0200

    second commit

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-if you are deploying to https://https://github.com/Cymekk.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:cymekk/spr.git main:gh-pages
 
 cd -
